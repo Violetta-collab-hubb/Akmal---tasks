6095825483006baa9f2c60c095781a328507d1c3
Akmal-collab-hub <VMYakubova@kpfu.ru>
2025-12-26 16:54:50 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-window.onload = function() {
-    setTimeout(showBio, 2000); // показываем биографию автоматически спустя 2 секунды
-};
+const choices = ['rock', 'paper', 'scissors'];
+let resultElement = document.querySelector('#result');
 
-function showBio() {
-    var bioElement = document.getElementById("bio");
-    bioElement.style.opacity = '1';
+// Генерируем случайный выбор компьютера
+function computerChoice() {
+    let randomIndex = Math.floor(Math.random() * choices.length); // получаем случайный индекс массива
+    return choices[randomIndex];
+}
+
+// Определяем победителя раунда
+function determineWinner(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) return "Ничья!";
+
+    switch (playerSelection + '-' + computerSelection) {
+        case 'rock-scissors':
+        case 'paper-rock':
+        case 'scissors-paper':
+            return "Победил игрок!";
+        default:
+            return "Компьютер победил.";
+    }
+}
+
+// Обработчик кликов по кнопкам
+function play(playerSelection) {
+    let computerSelected = computerChoice(); // Получаем выбор компьютера
+    let winnerMessage = determineWinner(playerSelection, computerSelected); // Определяем победителя
+
+    // Показываем результат игроку
+    resultElement.textContent = `${winnerMessage}\nВаш выбор: ${playerSelection}, Компьютер выбрал: ${computerSelected}.`;
 }
