5bb3a390f7baeb84f14e43cdc7d2867fb84b6305
Akmal-collab-hub <VMYakubova@kpfu.ru>
2025-12-26 16:55:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,28 @@
-window.onload = function() {
-    setTimeout(showBio, 2000); // показываем биографию автоматически спустя 2 секунды
+
+const changeLight = () => {
+    const lights = document.querySelectorAll('.light');
+    
+
+    let activeIndex = null;
+    for(let i = 0; i < lights.length; i++) {
+        if (!lights[i].classList.contains('off')) {
+            activeIndex = i;
+            break;
+        }
+    }
+    
+    if(activeIndex === null || activeIndex >= lights.length - 1){
+        activeIndex = 0;
+    } else {
+        activeIndex++;
+    }
+    
+    // Переключаем состояние светофора
+    lights.forEach((el, index) => {
+        el.classList.toggle('off', index !== activeIndex);
+    });
 };
 
-function showBio() {
-    var bioElement = document.getElementById("bio");
-    bioElement.style.opacity = '1';
-}
+// Запускаем переключение каждые 2 секунды
+setInterval(changeLight, 2000);
+changeLight(); // сразу включаем красный свет
